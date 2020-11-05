5ed164ef61f8694cbaae9c96b1d50da91353147e
Arthurttft <159753456852am>
2020-11-06 00:20:23 +0400

    problem1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,18 @@
 package com.company;
 
 import org.apache.xmlgraphics.ps.PSDictionaryFormatException;
+
 import java.util.Random;
 import java.lang.Math.*;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner Scan = new Scanner(System.in);
-        System.out.println("Pleas enter coordinat x");
-        int x = Scan.nextInt();
-        System.out.println("Pleas enter coordinat y");
-        int y = Scan.nextInt();
-        if (x = 4;){
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.println(i);
         }
+
     }
+
 }
