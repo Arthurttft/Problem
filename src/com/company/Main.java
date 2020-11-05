85e004736fb9384dc17f4dd16e7f2fb524773621
Arthurttft <159753456852am>
2020-11-05 19:17:47 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.apache.xmlgraphics.ps.PSDictionaryFormatException;
+import java.util.Random;
+import java.lang.Math.*;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Pleas enter coordinat x");
+        int x = Scan.nextInt();
+        System.out.println("Pleas enter coordinat y");
+        int y = Scan.nextInt();
+        if (x = 4;){
+        }
+    }
+}
